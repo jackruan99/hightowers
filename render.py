626ae2ce0c8401aaffc5ae54d582709933d47975
Jack Ruan <jackruan99@gmail.com>
2021-08-18 17:34:36 -0700

    done with render

--- render.py
+++ render.py
@@ -1,26 +1,40 @@
 import cairo
 
 
-def render(name, obstacles, width, height, path):
-    surface = cairo.SVGSurface(name + ".svg", width, height)
+def render(name, start, end, obstacles, width, height, path):
+    width, height = width + 2, height + 2
+    path = [] if path == None else path
+
+    scale = 100
+
+    surface = cairo.SVGSurface(name + ".svg", width*scale, height*scale)
     context = cairo.Context(surface)
-    context.scale(width, height)
-    context.set_line_width(0.04)
+    context.scale(scale, scale)
+    
+    # draw start and end point
+    context.set_line_width(0.05 * min(width, height))
+    context.set_source_rgb(0, 0, 0)
+    for x1, y1 in [start, end]:
+        context.arc(x1+1, y1+1, 0.1, 0, 2*3.14159)
+        context.fill()
+
     # draw the obstacles
+    context.set_line_width(0.01 * min(width, height))
+    context.set_source_rgb(255, 0, 0)
     for obstacle in obstacles:
         x1, y1 = obstacle[0]
         x2, y2 = obstacle[1]
-        context.move_to(x1, y1)
-        context.line_to(x2, y2)
+        context.move_to(x1+1, y1+1)
+        context.line_to(x2+1, y2+1)
         context.stroke()
-    # change color to green
-    context.set_source_rgb(0, 255, 0)
+    
     # draw the path
+    context.set_source_rgb(0, 255, 0)
     for segment in path:
         x1, y1 = segment[0]
         x2, y2 = segment[1]
-        context.move_to(x1, y1)
-        context.line_to(x2, y2)
+        context.move_to(x1+1, y1+1)
+        context.line_to(x2+1, y2+1)
         context.stroke()
 
     surface.finish()
--- test.py
+++ test.py
@@ -8,11 +8,13 @@ def run_test1():
     start = (0, 0)
     end = (0, 1)
     obstacles = []
-    width = 3
-    height = 3
-    print('expected:', [((0, 0), (0, 1))])
-    print('actual:  ', hightowers(start, end, obstacles, width, height))
-    render('test1', obstacles, width, height, hightowers(start, end, obstacles, width, height))
+    width = 1
+    height = 1
+    expected = [((0, 0), (0, 1))]
+    actual = hightowers(start, end, obstacles, width, height)
+    print('expected:', expected)
+    print('actual:  ', actual)
+    render('test1s', start, end, obstacles, width, height, actual)
 
 
 # TEST 2
@@ -24,8 +26,11 @@ def run_test2():
                  ((4, 4), (4, 0)), ((4, 0), (0, 0))]
     width = 4
     height = 4
-    print('expected:', [((1, 3), (3, 3)), ((3, 3), (3, 1))])
-    print('actual:  ', hightowers(start, end, obstacles, width, height))
+    expected = [((1, 3), (3, 3)), ((3, 3), (3, 1))]
+    actual = hightowers(start, end, obstacles, width, height)
+    print('expected:', expected)
+    print('actual:  ', actual)
+    render('test2s', start, end, obstacles, width, height, actual)
 
 
 # TEST 3
@@ -36,8 +41,11 @@ def run_test3():
     obstacles = [((1, 0), (1, 4)), ((1, 4), (4, 4)), ((4, 4), (4, 1))]
     width = 5
     height = 5
-    print('expected:', [((0, 4), (0, 5)), ((0, 5), (5, 5)), ((5, 5), (5, 0)), ((5, 0), (3, 0)), ((3, 0), (3, 3))])
-    print('actual:  ', hightowers(start, end, obstacles, width, height))
+    expected = [((0, 4), (0, 5)), ((0, 5), (5, 5)), ((5, 5), (5, 0)), ((5, 0), (3, 0)), ((3, 0), (3, 3))]
+    actual = hightowers(start, end, obstacles, width, height)
+    print('expected:', expected)
+    print('actual:  ', actual)
+    render('test3s', start, end, obstacles, width, height, actual)
 
 
 # TEST 4
@@ -49,8 +57,11 @@ def run_test4():
                  ((0, 1), (0, 7)), ((0, 7), (7, 7)), ((7, 7), (7, 0)), ((7, 0), (2, 0))]
     width = 7
     height = 7
-    print('expected:', [((1, 5), (1, 6)), ((1, 6), (6, 6)), ((6, 6), (6, 1)), ((6, 1), (4, 1)), ((4, 1), (4, 4))])
-    print('actual:  ', hightowers(start, end, obstacles, width, height))
+    expected = [((1, 5), (1, 7)), ((1, 6), (6, 6)), ((6, 6), (6, 1)), ((6, 1), (4, 1)), ((4, 1), (4, 4))]
+    actual = hightowers(start, end, obstacles, width, height)
+    print('expected:', expected)
+    print('actual:  ', actual)
+    render('test4s', start, end, obstacles, width, height, actual)
 
 
 # TEST 5
@@ -60,11 +71,13 @@ def run_test5():
     end = (3, 2)
     obstacles = [((2, 1), (2, 3)), ((2, 3), (4, 3)), ((4, 3), (4, 1)), ((4, 1), (2, 1)), ((
         0, 0), (0, 4)), ((0, 4), (5, 4)), ((5, 4), (5, 0)), ((5, 0), (0, 0))]
-    width = 6
-    height = 5
-    print('expected:', None)
-    print('actual:  ', hightowers(start, end, obstacles, width, height))
-
+    width = 5
+    height = 4
+    expected = None
+    actual = hightowers(start, end, obstacles, width, height)
+    print('expected:', expected)
+    print('actual:  ', actual)
+    render('test5s', start, end, obstacles, width, height, actual)
 
 
 # TEST 6
@@ -73,10 +86,13 @@ def run_test6():
     start = (0, 5)
     end = (4, 5)
     obstacles = [((1, 5), (1, 9)), ((3, 8), (3, 0))]
-    width = 5
-    height = 10
-    print('expected:', [((0, 5), (0, 4)), ((0, 4), (2, 4)), ((2, 4), (2, 9)), ((2, 9), (4, 9)), ((4, 9), (4, 5))])
-    print('actual:  ', hightowers(start, end, obstacles, width, height))
+    width = 4
+    height = 9
+    expected = [((0, 5), (0, 4)), ((0, 4), (2, 4)), ((2, 4), (2, 9)), ((2, 9), (4, 9)), ((4, 9), (4, 5))]
+    actual = hightowers(start, end, obstacles, width, height)
+    print('expected:', expected)
+    print('actual:  ', actual)
+    render('test6s', start, end, obstacles, width, height, actual)
 
 
 # TEST 7
@@ -88,8 +104,11 @@ def run_test7():
         1, 11), (8, 11)), ((8, 5), (12, 5)), ((9, 8), (11, 8)), ((11, 8), (11, 12))]
     width = 12
     height = 12
-    print('expected:', [((0, 0), (0, 5)), ((0, 5), (5, 5)), ((5, 5), (5, 10)), ((5, 10), (7, 10)), ((7, 10), (7, 6)), ((7, 6), (12, 6)), ((12, 6), (12, 12))])
-    print('actual:  ', hightowers(start, end, obstacles, width, height))
+    expected = [((0, 0), (0, 5)), ((0, 5), (5, 5)), ((5, 5), (5, 10)), ((5, 10), (7, 10)), ((7, 10), (7, 6)), ((7, 6), (12, 6)), ((12, 6), (12, 12))]
+    actual = hightowers(start, end, obstacles, width, height)
+    print('expected:', expected)
+    print('actual:  ', actual)
+    render('test7s', start, end, obstacles, width, height, actual)
 
 
 # TEST 8
@@ -99,17 +118,20 @@ def run_test8():
     end = (7, 7)
     obstacles = [((0, 0), (0, 8)), ((0, 8), (8, 8)), ((8, 8), (8, 0)), ((8, 0), (0, 0)), ((
         2, 0), (2, 5)), ((2, 5), (4, 5)), ((2, 8), (2, 7)), ((2, 7), (4, 7)), ((4, 2), (6, 2)), ((6, 2), (6, 8))]
-    width = 9
-    height = 9
-    print('expected:', [((1, 1), (1, 6)), ((1, 6), (5, 6)), ((5, 6), (5, 4)), ((5, 4), (3, 4)), ((3, 4), (3, 1)), ((3, 1), (7, 1)), ((7, 1), (7, 7))])
-    print('actual:  ', hightowers(start, end, obstacles, width, height))
+    width = 8
+    height = 8
+    expected = [((1, 1), (1, 6)), ((1, 6), (5, 6)), ((5, 6), (5, 4)), ((5, 4), (3, 4)), ((3, 4), (3, 1)), ((3, 1), (7, 1)), ((7, 1), (7, 7))]
+    actual = hightowers(start, end, obstacles, width, height)
+    print('expected:', expected)
+    print('actual:  ', actual)
+    render('test8s', start, end, obstacles, width, height, actual)
 
 
 run_test1()  # pass
-# run_test2()  # pass
-# run_test3()  # pass, need post editing
-# run_test4()  # pass, need post editing
-# run_test5()  # pass
-# run_test6()  # pass
-# run_test7()  # pass, need post editing
-# run_test8()  # pass, need post editing
+run_test2()  # pass
+run_test3()  # pass, need post editing
+run_test4()  # pass, need post editing
+run_test5()  # pass
+run_test6()  # pass
+run_test7()  # pass, need post editing
+run_test8()  # pass, need post editing
